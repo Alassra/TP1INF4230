96dd49b4e1acf9ae0ce0e5b33dc5f2bfe5bc00a7
who-am-i-now <46800394+who-am-i-now@users.noreply.github.com>
2019-02-24 22:09:09 -0500

    Add files via upload

---
+++ HeuristiqueSimple.java
@@ -0,0 +1,19 @@
+
+public class HeuristiqueSimple extends Heuristique {
+
+	public HeuristiqueSimple(Urgence urgence) {
+		super(urgence);
+	}
+
+	public double estimerCoutRestant(Etat etat, But but) {
+		if (but.butEstStatisfait(etat)) {
+			return 0;
+		}
+		double min = Double.MAX_VALUE;
+		for (Emplacement e : etat.emplacementsPatients) {
+			double delta = e.positionGeographique.distance(etat.emplacementAmbulance.positionGeographique);
+			min = Math.min(min, delta);
+		}
+		return min;
+	}
+}
